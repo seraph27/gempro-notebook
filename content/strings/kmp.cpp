aa2fd364967ef52a6c30657eed553604e15886c0
Miguel <migueldco14@gmail.com>
2025-11-13 17:26:18 -0300

    make kmp more compact

--- content/strings/kmp.cpp
+++ content/strings/kmp.cpp
@@ -8,9 +8,7 @@ vector<int> kmp(string s) {
 	int n = s.size(), len = 0;
 	vector<int> pre(n);
 	for (int i = 1; i < n; i++) {
-		while (len > 0 && s[i] != s[len]) {
-			len = pre[len - 1];
-		}
+		while (len > 0 && s[i] != s[len]) len = pre[len - 1];
 		if (s[i] == s[len]) len++;
 		pre[i] = len;
 	}
